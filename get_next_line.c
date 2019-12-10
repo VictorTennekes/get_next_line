da482e983856806b2aa27a9bcb705f2dd637f9eb
Victor jan piet joostszoon Tennekes <vtenneke@f0r1s8.codam.nl>
2019-12-10 13:29:03 +0100

    finished version

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,12 @@
 /*   By: vtenneke <vtenneke@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/27 10:07:26 by vtenneke       #+#    #+#                */
-/*   Updated: 2019/12/09 09:36:27 by vtenneke      ########   odam.nl         */
+/*   Updated: 2019/12/10 10:27:12 by vtenneke      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
@@ -35,6 +36,8 @@ int		get_line(char **res, char **line, int c)
 {
 	char	*tmp;
 
+	if (line)
+		free(*line);
 	*line = ft_substr(*res, 0, ft_strchr(*res, c) + ((c == '\0') ? 1 : -1));
 	if (c == '\0')
 	{
